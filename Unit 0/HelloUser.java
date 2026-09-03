81bc9d01799d8dc6628b77ece83fabc7f90b4454
Joel <jhammer@friendsbalt.org>
2026-09-02 22:31:08 -0400

    Add hello world

---
+++ Unit 0/HelloUser.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HelloUser {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        String userName = input.next();
+
+        System.out.println("Hello, " + userName + "!");
+
+        input.close();
+    }
+}
